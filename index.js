cb4eb73d84532d9231d0a2467f998532b756f92e
Nikolenko Front End Pro 24.01.23 <nikolenkoekaterina@gmail.com>
2023-02-01 12:03:43 +0200

    second version

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 let elements = prompt("Enter the elements");
 let arr = elements.split(', ');
 
-for(let j = 0; j < arr.length; j++){
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] > arr[i + 1]){
-            let element = arr[i];
-            arr[i] = arr[i + 1];
-            arr[i + 1] = element;
-        }
-        if(arr[i] < arr[i - 1]){
-            let element = arr[i];
-            arr[i] = arr[i - 1];
-            arr[i - 1] = element;
-        }
-    }
-}
+//     first version
+
+// for(let j = 0; j < arr.length; j++){
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] > arr[i + 1]){
+//             let element = arr[i];
+//             arr[i] = arr[i + 1];
+//             arr[i + 1] = element;
+//         }
+//         if(arr[i] < arr[i - 1]){
+//             let element = arr[i];
+//             arr[i] = arr[i - 1];
+//             arr[i - 1] = element;
+//         }
+//     }
+// }
+
+//     second version
+
+arr.sort();
 console.log(arr);
 
 arr.splice(2, 3);
